d6a40d1b6abd3ab1d3fb1bd0676c935dac2af1f4
CWRalph <colwynralph15@gmail.com>
2021-12-22 14:15:38 -0800

    Added the code that updates with the user's location and added a button that can print it for testing purposes

--- app/src/main/java/com/example/ping4/Compass.java
+++ app/src/main/java/com/example/ping4/Compass.java
@@ -1,29 +1,83 @@
 package com.example.ping4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Compass extends AppCompatActivity {
 
-    public void maps(View view){
+    LocationManager locationManager;
+    LocationListener locationListener;
+    double latitude;
+    double longitude;
+
+    public void maps(View view) {
         //Creates an intent which is the other activity
-        Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
         //Start activity activates the intent
         startActivity(intent);
     }
-    public void compass(View view){
-        Toast.makeText(this,"Already on compass page!", Toast.LENGTH_SHORT).show();
+    public void printPosition(View view){
+        Toast.makeText(this,"Latitude: "+ Double.toString(latitude) + ", Longitude: " + Double.toString(longitude),Toast.LENGTH_SHORT).show();
     }
 
+    public void compass(View view) {
+        Toast.makeText(this, "Already on compass page!", Toast.LENGTH_SHORT).show();
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                updateLocationInfo(location);
+            }
+        };
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnownLocation != null) {
+                updateLocationInfo(lastKnownLocation);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
+    }
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public void updateLocationInfo(Location location){
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
     }
 }
